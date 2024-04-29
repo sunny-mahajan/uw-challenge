e206fe91b99801de43e7cbf03fae78ec25758557
Sunny kumar <sunny@techformation.co.in>
2024-04-29 16:18:16 +0530

    set redirections to chat

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -12,11 +12,6 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
-                    </x-nav-link>
-                </div>
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('chat')" :active="request()->routeIs('chat')">
                         {{ "Chat" }}
                     </x-nav-link>
--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,6 @@ use App\Http\Controllers\ChatController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
